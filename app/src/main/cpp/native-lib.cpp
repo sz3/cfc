1b043d9e747900b04f14ccd33bcf0ddc3aeeba7e
sz <sz@galacticicecube.com>
2020-04-24 16:55:19 -0500

    nope

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,17 @@
 #include <jni.h>
+
+#include "opencv2/opencv.hpp"
 #include <string>
+#include <sstream>
+
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_example_camerafilecopy_MainActivity_stringFromJNI(
-        JNIEnv* env,
-        jobject /* this */) {
-    std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+		JNIEnv* env,
+		jobject /* this */) {
+
+	std::stringstream ss;
+	ss << "cpus: " << cv::getNumberOfCPUs() << " , ticks: " << cv::getTickCount();
+	std::string hello = "Hello from C++! " + ss.str();
+	return env->NewStringUTF(hello.c_str());
 }
