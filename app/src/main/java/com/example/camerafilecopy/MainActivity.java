95ad000f9e7400d1f670a47cfd804fcdf71b0739
sz <sz@galacticicecube.com>
2020-04-23 19:09:52 -0500

    No BC with old android for now, android dev is enough of a pain as is

--- app/src/main/java/com/example/camerafilecopy/MainActivity.java
+++ app/src/main/java/com/example/camerafilecopy/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.camerafilecopy;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
 import android.widget.TextView;
 
