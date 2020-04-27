72d1c5b2cdf29fa77336240730d2f421b72f68c6
sz <sz@galacticicecube.com>
2020-04-27 15:44:31 -0500

    Specify c++17 in build config, and add more logging

--- app/src/main/cpp/lib/native-lib/native-lib.cpp
+++ app/src/main/cpp/lib/native-lib/native-lib.cpp
@@ -11,9 +11,17 @@
 using namespace std;
 using namespace cv;
 
+namespace {
+    unsigned _calls = 0;
+    unsigned _successfulScans = 0;
+    unsigned long long _scanTicks;
+}
+
 extern "C" {
 void JNICALL
 Java_com_example_camerafilecopy_MainActivity_adaptiveThresholdFromJNI(JNIEnv *env, jobject instance, jlong matAddr) {
+
+    ++_calls;
 
     // get Mat from raw address
     Mat &mat = *(Mat *) matAddr;
@@ -22,11 +30,21 @@ Java_com_example_camerafilecopy_MainActivity_adaptiveThresholdFromJNI(JNIEnv *en
 
     Processing p;
     std::vector<Anchor> anchors = p.scan(mat);
-    std::stringstream ss;
-    ss << "hello. " << anchors.size();
+    if (anchors.size() > 3)
+    {
+        ++_successfulScans;
+        _scanTicks += (clock() - begin);
+    }
+
+    std::stringstream sstop;
+    sstop << "hi. " << anchors.size() << " anchors. " << _scanTicks;
+
+    std::stringstream ssmid;
+    ssmid << _successfulScans << " / " << _calls;
 
     cv::adaptiveThreshold(mat, mat, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 21, 5);
-    cv::putText(mat, ss.str(), cv::Point(5,100), FONT_HERSHEY_DUPLEX, 1, cv::Scalar(255,255,80), 2);
+    cv::putText(mat, sstop.str(), cv::Point(5,50), FONT_HERSHEY_DUPLEX, 1, cv::Scalar(255,255,80), 2);
+    cv::putText(mat, ssmid.str(), cv::Point(5,100), FONT_HERSHEY_DUPLEX, 1, cv::Scalar(255,255,80), 2);
 
     // log computation time to Android Logcat
     double totalTime = double(clock() - begin) / CLOCKS_PER_SEC;
