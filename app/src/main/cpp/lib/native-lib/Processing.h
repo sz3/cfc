6b2677f7a7d686d673948b44234d81ebde033fea
sz <sz@galacticicecube.com>
2020-04-29 19:29:34 -0500

    Trying to add decoder stuff, not super optimistic

--- app/src/main/cpp/lib/native-lib/Processing.h
+++ app/src/main/cpp/lib/native-lib/Processing.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include "encoder/Decoder.h"
 #include "extractor/Anchor.h"
+#include "extractor/Corners.h"
+#include "extractor/Deskewer.h"
 #include "extractor/Scanner.h"
 
 #include <opencv2/opencv.hpp>
@@ -9,12 +12,42 @@
 class Processing
 {
 public:
-    Processing() {}
+	Processing() {}
 
-    std::vector<Anchor> scan(const cv::Mat& img)
-    {
-        Scanner scanner(img);
-        return scanner.scan();
-    }
+	std::vector<Anchor> scan(cv::Mat& mat)
+	{
+		Scanner scanner(mat);
+		std::vector<Anchor> anchors = scanner.scan();
+
+		std::stringstream sstop;
+		sstop << mat.rows << " x " << mat.cols << " : " << anchors.size() << " anchors.";
+		cv::putText(mat, sstop.str(), cv::Point(5,50), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(255,255,80), 2);
+		return anchors;
+	}
+
+	cv::Mat extract(cv::Mat& mat, const std::vector<Anchor>& points)
+	{
+		Corners corners(points[0].center(), points[1].center(), points[2].center(), points[3].center());
+		Deskewer de;
+		cv::Mat out = de.deskew(mat, corners);
+		cv::rotate(out, out, cv::ROTATE_90_CLOCKWISE);
+
+		std::stringstream sstop;
+		sstop << out.rows << " x " << out.cols;
+		cv::putText(mat, sstop.str(), cv::Point(5,100), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(255,255,80), 2);
+		return out;
+	}
+
+	unsigned decode(cv::Mat& mat, const cv::Mat& img)
+	{
+		Decoder de(0);
+		unsigned res = de.decode(img, "/dev/shm/camerafilecopy.tmp.txt");
+
+		std::stringstream sstop;
+		sstop << "decoded " << res << " bytes.";
+		cv::putText(mat, sstop.str(), cv::Point(5,150), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(255,255,80), 2);
+		return res;
+	}
+
 protected:
 };
--- app/src/main/cpp/lib/native-lib/native-lib.cpp
+++ app/src/main/cpp/lib/native-lib/native-lib.cpp
@@ -14,7 +14,9 @@ using namespace cv;
 namespace {
 	unsigned _calls = 0;
 	unsigned _successfulScans = 0;
-	unsigned long long _scanTicks;
+	unsigned long long _scanTicks = 0;
+	unsigned long long _extractTicks = 0;
+	unsigned long long _decodeTicks = 0;
 }
 
 extern "C" {
@@ -34,27 +36,29 @@ Java_com_example_camerafilecopy_MainActivity_adaptiveThresholdFromJNI(JNIEnv *en
 	{
 		++_successfulScans;
 		_scanTicks += (clock() - begin);
+
+		cv::Mat img = p.extract(mat, anchors);
+		_extractTicks += (clock() - begin);
+
+		p.decode(mat, img);
+		_decodeTicks += (clock() - begin);
 	}
 
-    if (_calls % 10 == 0 and anchors.size() > 0 and anchors.size() < 4)
-    {
-        std::stringstream fname;
-        fname << "/storage/emulated/0/Android/data/com.example.camerafilecopy/files/myimage" << _calls << ".png";
-        cv::imwrite(fname.str(), mat);
-    }
-
-	std::stringstream sstop;
-	sstop << mat.rows << " x " << mat.cols << " : " << anchors.size() << " anchors. " << _scanTicks;
+	if (_calls % 10 == 0 and anchors.size() > 0 and anchors.size() < 4)
+	{
+		std::stringstream fname;
+		fname << "/storage/emulated/0/Android/data/com.example.camerafilecopy/files/myimage" << _calls << ".png";
+		//cv::imwrite(fname.str(), mat);
+	}
 
 	std::stringstream ssmid;
-	ssmid << _successfulScans << " / " << _calls;
+	ssmid << "success: " << _successfulScans << " / " << _calls << ". scan: " << _scanTicks << ", extract: " << _extractTicks << ", decode: " << _decodeTicks;
 
 	//cv::adaptiveThreshold(mat, mat, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 21, 5);
-	cv::putText(mat, sstop.str(), cv::Point(5,50), FONT_HERSHEY_DUPLEX, 1, cv::Scalar(255,255,80), 2);
-	cv::putText(mat, ssmid.str(), cv::Point(5,100), FONT_HERSHEY_DUPLEX, 1, cv::Scalar(255,255,80), 2);
+	cv::putText(mat, ssmid.str(), cv::Point(5,150), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(255,255,80), 2);
 
 	for (const Anchor& anchor : anchors)
-		cv::rectangle(mat, cv::Point(anchor.x(), anchor.y()), cv::Point(anchor.xmax(), anchor.ymax()), cv::Scalar(255,20,20));
+		cv::rectangle(mat, cv::Point(anchor.x(), anchor.y()), cv::Point(anchor.xmax(), anchor.ymax()), cv::Scalar(255,20,20), 10);
 
 	// log computation time to Android Logcat
 	double totalTime = double(clock() - begin) / CLOCKS_PER_SEC;
