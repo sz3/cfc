e440dc1c6ab5da1cfa228f2cfdfa40234cc79ac4
sz <sz@galacticicecube.com>
2020-05-06 16:52:46 -0500

    Squashed 'app/src/main/cpp/lib/libcimbar/' changes from b4929ad..d8abaa2
    d8abaa2 Small fixes
    abe7b32 Add pause/resume
    2464991 An experiment to limit the size of the work queue for threadpool
    92ca89b Add CMakeLists.txt for various third party header components
    579be2d Copy over turbolib's thread_pool, and add concurrentqueue
    af3d36b Move the CimbDecoder up a level, so we only need to initialize it once ...
    
    git-subtree-dir: app/src/main/cpp/lib/libcimbar
    git-subtree-split: d8abaa26e44f217ec2032038a2f16d8ddc57b1a0

---
+++ src/lib/concurrent/test/thread_poolTest.cpp
@@ -0,0 +1,97 @@
+/* This code is subject to the terms of the Mozilla Public License, v.2.0. http://mozilla.org/MPL/2.0/. */
+#include "unittest.h"
+
+#include "thread_pool.h"
+
+#include "concurrent/monitor.h"
+#include "serialize/str_join.h"
+#include <algorithm>
+#include <iostream>
+#include <mutex>
+#include <vector>
+using std::lock_guard;
+using std::mutex;
+using turbo::monitor;
+
+TEST_CASE( "thread_poolTest/testDefault", "[unit]" )
+{
+	turbo::thread_pool threads(5);
+	assertTrue( threads.start() );
+
+	mutex myMutex;
+	std::vector<unsigned> results;
+	for (unsigned i = 0; i < 10; ++i)
+		threads.execute( [&, i] () { lock_guard<mutex> lock(myMutex); results.push_back(i); } );
+
+	monitor m;
+	threads.execute( [&m] () { m.signal_all(); } );
+	assertTrue( m.wait_for(1000) );
+
+	while (results.size() < 10)
+		std::cout << threads.queued() << std::endl;
+
+	std::sort(results.begin(), results.end());
+	assertEquals( "0 1 2 3 4 5 6 7 8 9", turbo::str::join(results) );
+}
+
+
+TEST_CASE( "thread_poolTest/testTryExecute", "[unit]" )
+{
+	turbo::thread_pool threads(3, 1);
+
+	mutex myMutex;
+	std::vector<unsigned> results;
+
+	// fill up the queue
+	for (unsigned i = 0; i < 64; ++i)
+		assertTrue( threads.try_execute( [&, i] () { lock_guard<mutex> lock(myMutex); results.push_back(i); } ) );
+
+	// can't schedule any more
+	for (unsigned i = 64; i < 70; ++i)
+		assertFalse( threads.try_execute( [&, i] () { lock_guard<mutex> lock(myMutex); results.push_back(i); } ) );
+
+	assertTrue( threads.start() );
+	monitor m;
+	threads.execute( [&m] () { m.signal_all(); } );
+	assertTrue( m.wait_for(1000) );
+
+	while (results.size() < 10)
+		std::cout << threads.queued() << std::endl;
+
+	std::sort(results.begin(), results.end());
+	assertStringContains( "0 1 2 3 4 5 6 7 8 9", turbo::str::join(results) );
+	assertEquals( 64, results.size() );
+}
+
+TEST_CASE( "thread_poolTest/testPause", "[unit]" )
+{
+	// uses try_execute
+	turbo::thread_pool threads(3, 1);
+	assertTrue( threads.start() );
+	threads.pause();
+
+	mutex myMutex;
+	std::vector<unsigned> results;
+
+	// fill up the queue
+	for (unsigned i = 0; i < 64; ++i)
+		assertTrue( threads.try_execute( [&, i] () { lock_guard<mutex> lock(myMutex); results.push_back(i); } ) );
+
+	// with nothing running, we can't schedule any more
+	for (unsigned i = 64; i < 70; ++i)
+		assertFalse( threads.try_execute( [&, i] () { lock_guard<mutex> lock(myMutex); results.push_back(i); } ) );
+
+	threads.resume();
+
+	monitor m;
+	threads.execute( [&m] () { m.signal_all(); } );
+	assertTrue( m.wait_for(2000) );
+
+	while (results.size() < 10)
+		std::cout << threads.queued() << std::endl;
+
+	std::sort(results.begin(), results.end());
+	assertStringContains( "0 1 2 3 4 5 6 7 8 9", turbo::str::join(results) );
+	assertEquals( 64, results.size() );
+}
+
