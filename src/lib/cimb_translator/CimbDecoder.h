c6f051e0af0ad320ed045dea11cbc86801f55f39
sz <sz@galacticicecube.com>
2020-05-05 20:42:10 -0500

    Squashed 'app/src/main/cpp/lib/libcimbar/' changes from 9894339..b4929ad
    b4929ad Merge pull request #2 from sz3/faster-fuzzy-hash
    b5266fd Cleanup
    77ad5e2 Put it back.
    0253a2c This may or may not be a good idea
    64de835 Invert the hash order again...
    75f8cc5 Iterate by img ptr to (maybe) speed up the image hash a bit
    58ef4d2 Playing with using opencv classes a bit less.
    1e2d1d0 Minor improvements, maybe, for CellDrift class
    05e9737 An even more aggressively optimized mean_color
    fbaaa8b Uncessary optimization that notably improves performance on android
    72fabfe Check in intx's "int128"
    622d0fd Check 6x6 block for avg color.
    cb637f2 Use intx?
    76483b4 Make bitset_extractor ambivalent(-ish) about the underlying type
    e3a099d Split the fuzzy_ahash function into two parts for now.
    
    git-subtree-dir: app/src/main/cpp/lib/libcimbar
    git-subtree-split: b4929ad929e97985a35d43b3f701641999dab164

--- src/lib/cimb_translator/CimbDecoder.h
+++ src/lib/cimb_translator/CimbDecoder.h
@@ -12,7 +12,7 @@ public:
 	unsigned decode(const cv::Mat& color_cell);
 	unsigned decode(const cv::Mat& cell, const cv::Mat& color_cell, unsigned& drift_offset);
 
-	unsigned get_best_symbol(const std::vector<uint64_t>& hashes, unsigned& best_distance);
+	unsigned get_best_symbol(const std::array<uint64_t,9>& hashes, unsigned& best_distance);
 	unsigned decode_symbol(const cv::Mat& cell, unsigned& drift_offset);
 
 	unsigned get_best_color(unsigned char r, unsigned char g, unsigned char b) const;
@@ -22,7 +22,7 @@ protected:
 	uint64_t get_tile_hash(unsigned symbol);
 	bool load_tiles();
 
-	unsigned check_color_distance(cv::Vec3b c, unsigned char r, unsigned char g, unsigned char b) const;
+	unsigned check_color_distance(std::tuple<uchar,uchar,uchar> c, unsigned char r, unsigned char g, unsigned char b) const;
 	unsigned char fix_color(unsigned char c, float adjust) const;
 
 protected:
