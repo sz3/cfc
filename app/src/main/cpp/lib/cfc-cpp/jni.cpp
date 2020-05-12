6c23ad649dc423db487ddfe603eb6d246439943e
sz <sz@galacticicecube.com>
2020-05-12 04:13:42 -0500

    Squashed android-specific changes.
    (libcimbar subtree next)

---
+++ app/src/main/cpp/lib/cfc-cpp/jni.cpp
@@ -0,0 +1,85 @@
+#include "Processing.h"
+
+#include <jni.h>
+#include <android/log.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <sstream>
+
+#define TAG "CameraFileCopyCPP"
+
+using namespace std;
+using namespace cv;
+
+namespace {
+	unsigned _calls = 0;
+	unsigned _successfulScans = 0;
+	unsigned long long _scanTicks = 0;
+	unsigned long long _extractTicks = 0;
+	unsigned long long _decodeTicks = 0;
+}
+
+extern "C" {
+void JNICALL
+Java_com_galacticicecube_camerafilecopy_MainActivity_processImageJNI(JNIEnv *env, jobject instance, jlong matAddr) {
+
+	++_calls;
+
+	// get Mat from raw address
+	Mat &mat = *(Mat *) matAddr;
+
+	clock_t begin = clock();
+
+	Processing p;
+	std::vector<Anchor> anchors = p.scan(mat);
+	if (anchors.size() > 3)
+	{
+		++_successfulScans;
+		_scanTicks += (clock() - begin);
+
+		cv::Mat img = p.extract(mat, anchors);
+		_extractTicks += (clock() - begin);
+
+		p.decode(mat, img);
+		_decodeTicks += (clock() - begin);
+	}
+
+	if (_calls % 10 == 0 and anchors.size() > 0 and anchors.size() < 4)
+	{
+		std::stringstream fname;
+		fname << "/storage/emulated/0/Android/data/com.galacticicecube.camerafilecopy/files/myimage" << _calls << ".png";
+		//cv::imwrite(fname.str(), mat);
+	}
+
+	std::stringstream ssmid;
+	ssmid << "success: " << _successfulScans << " / " << _calls << ". scan: " << _scanTicks << ", extract: " << _extractTicks << ", decode: " << _decodeTicks;
+
+	//cv::adaptiveThreshold(mat, mat, 255, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, 21, 5);
+	cv::putText(mat, ssmid.str(), cv::Point(5,150), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(255,255,80), 2);
+
+	for (const Anchor& anchor : anchors)
+		cv::rectangle(mat, cv::Point(anchor.x(), anchor.y()), cv::Point(anchor.xmax(), anchor.ymax()), cv::Scalar(255,20,20), 10);
+
+	// log computation time to Android Logcat
+	double totalTime = double(clock() - begin) / CLOCKS_PER_SEC;
+	__android_log_print(ANDROID_LOG_INFO, TAG, "adaptiveThreshold computation time = %f seconds\n",
+						totalTime);
+}
+
+void JNICALL
+Java_com_galacticicecube_camerafilecopy_MainActivity_pauseJNI(JNIEnv *env, jobject instance) {
+    __android_log_print(ANDROID_LOG_INFO, TAG, "Pause cfc-cpp\n");
+}
+
+void JNICALL
+Java_com_galacticicecube_camerafilecopy_MainActivity_resumeJNI(JNIEnv *env, jobject instance) {
+    __android_log_print(ANDROID_LOG_INFO, TAG, "Resume cfc-cpp\n");
+}
+
+
+void JNICALL
+Java_com_galacticicecube_camerafilecopy_MainActivity_shutdownJNI(JNIEnv *env, jobject instance) {
+    __android_log_print(ANDROID_LOG_INFO, TAG, "Shutdown cfc-cpp\n");
+}
+
+}
---
+++ app/src/main/java/com/galacticicecube/camerafilecopy/MainActivity.java
@@ -0,0 +1,143 @@
+package com.galacticicecube.camerafilecopy;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import androidx.core.app.ActivityCompat;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.WindowManager;
+import android.widget.Toast;
+
+import org.jetbrains.annotations.NotNull;
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Mat;
+
+public class MainActivity extends Activity implements CvCameraViewListener2 {
+    private static final String TAG = "MainActivity";
+    private static final int CAMERA_PERMISSION_REQUEST = 1;
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            if (status == LoaderCallbackInterface.SUCCESS) {
+                Log.i(TAG, "OpenCV loaded successfully");
+
+                // Load native library after(!) OpenCV initialization
+                System.loadLibrary("cfc-cpp");
+
+                mOpenCvCameraView.enableView();
+            } else {
+                super.onManagerConnected(status);
+            }
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        // Permissions for Android 6+
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{Manifest.permission.CAMERA},
+                CAMERA_PERMISSION_REQUEST
+        );
+
+        ActivityCompat.requestPermissions(
+                this,
+                new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                2
+        );
+
+        String path = this.getExternalFilesDir(null).getPath();
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        setContentView(R.layout.activity_main);
+
+        mOpenCvCameraView = findViewById(R.id.main_surface);
+
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NotNull String[] permissions, @NotNull int[] grantResults) {
+        if (requestCode == CAMERA_PERMISSION_REQUEST) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                mOpenCvCameraView.setCameraPermissionGranted();
+            } else {
+                String message = "Camera permission was not granted";
+                Log.e(TAG, message);
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+            }
+        } else {
+            Log.e(TAG, "Unexpected permission request");
+        }
+    }
+
+    @Override
+    public void onPause() {
+        pauseJNI();
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+        resumeJNI();
+    }
+
+    @Override
+    public void onDestroy() {
+        shutdownJNI();
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+    }
+
+    @Override
+    public Mat onCameraFrame(CvCameraViewFrame frame) {
+        // get current camera frame as OpenCV Mat object
+        Mat mat = frame.rgba();
+
+        // native call to process current camera frame
+        processImageJNI(mat.getNativeObjAddr());
+
+        // return processed frame for live preview
+        return mat;
+    }
+
+    private native void processImageJNI(long mat);
+    private native void pauseJNI();
+    private native void resumeJNI();
+    private native void shutdownJNI();
+}
