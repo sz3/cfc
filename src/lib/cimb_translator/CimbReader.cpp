7a6c08fc3617285125cfd67325b5468f5d1821dd
sz <sz@galacticicecube.com>
2021-02-17 01:40:04 -0600

    Squashed 'app/src/cpp/libcimbar/' changes from 8298ead4..9b90e78a
    9b90e78a Merge pull request #53 from sz3/ccm
    b69a6445 Don't need this?
    8f2830ad comments
    c0b02f82 configurable redundancy for encode_fountain().
    02af3063 Update submodule rev
    ab0754fe A test against a cimbar code that really needs color correction
    1f27016a Add some simple tests for CCM...
    d3656770 Use float for now?
    8d5a637e Formatting, and hopefully the compiler does what I want
    1d5aa984 Slight refactor, and a cli option to turn color correction off
    676fd35b Don't need it!
    c8802dc5 Merge remote-tracking branch 'origin/master' into ccm
    470f1283 Merge branch 'optimize' into ccm
    717b4b02 Merge pull request #52 from sz3/optimize
    71f2e74b argh
    74f5a540 Small docs updates
    507418a6 Change case for this guy
    17726772 Add a simpler test for CimbReader
    df91a77b More clarity for performance #s
    ae5abc70 Update CimbDecoder tests for interface change.
    e1d947d7 ok clang
    fa2b17ac We're now operating on an 8x8
    b21d37c8 Brittle regression test
    b5112c9b The experiment continues
    7ad221b3 WIP?
    6bf1caa2 Types, and got some BGR in my RGB
    3d03a862 We *should* be able to use smaller types for the flood-fill decode heap
    ba0f00cf A stab at light mode
    0dd82e85 First pass at color correction.
    9e731c11 Matx
    5f12fa0d color correction matrix stuff
    a22f332c Merge pull request #50 from sz3/cimbarjs-bugfix
    cccdd366 Fix typo, improve doc
    722f6303 Hide the nav menu when a file is loaded
    a678f39b A little flair
    
    git-subtree-dir: app/src/cpp/libcimbar
    git-subtree-split: 9b90e78a353d26c38c9cee04fbabe10e44a6176a

--- src/lib/cimb_translator/CimbReader.cpp
+++ src/lib/cimb_translator/CimbReader.cpp
@@ -5,6 +5,8 @@
 #include "Config.h"
 
 #include "bit_file/bitmatrix.h"
+#include "chromatic_adaptation/adaptation_transform.h"
+#include "chromatic_adaptation/color_correction.h"
 #include <opencv2/opencv.hpp>
 
 using namespace cimbar;
@@ -43,9 +45,53 @@ namespace {
 		bitmatrix::mat_to_bitbuffer(symbols, bb.get_writer());
 		return bb;
 	}
+
+	void updateMaxColor(std::tuple<float, float, float>& max_color, const cv::Scalar& c)
+	{
+		std::get<0>(max_color) = std::max(std::get<0>(max_color), static_cast<float>(c[0]));
+		std::get<1>(max_color) = std::max(std::get<1>(max_color), static_cast<float>(c[1]));
+		std::get<2>(max_color) = std::max(std::get<2>(max_color), static_cast<float>(c[2]));
+	}
+
+	std::tuple<float, float, float> calculateWhite(const cv::Mat& img, bool dark)
+	{
+		std::tuple<float, float, float> bestColor({1, 1, 1});
+		if (dark)
+		{
+			unsigned tl = Config::anchor_size() - 2;
+			unsigned br = Config::image_size() - Config::anchor_size() - 2;
+			std::array<std::pair<unsigned, unsigned>, 3> anchors = {{ {tl, tl}, {tl, br}, {br, tl} }};
+			for (auto [x, y] : anchors)
+			{
+				cv::Rect crop(x, y, 4, 4);
+				cv::Scalar avgColor = cv::mean(img(crop));
+				updateMaxColor(bestColor, avgColor);
+			}
+		}
+		else // light
+		{
+			unsigned tl = (Config::anchor_size() << 1) + 6;
+			unsigned br = Config::image_size() - tl - 4;
+			std::array<std::pair<unsigned, unsigned>, 4> anchors = {{ {0, tl}, {tl, 0}, {0, br}, {br, 0} }};
+			for (auto [x, y] : anchors)
+			{
+				cv::Rect crop(x, y, 4, 4);
+				cv::Scalar avgColor = cv::mean(img(crop));
+				updateMaxColor(bestColor, avgColor);
+			}
+		}
+		return bestColor;
+	}
+
+	bool updateColorCorrection(const cv::Mat& img, CimbDecoder& decoder)
+	{
+		std::tuple<float, float, float> white = calculateWhite(img, Config::dark());
+		decoder.update_color_correction(color_correction::get_adaptation_matrix<adaptation_transform::von_kries>(white, {255.0, 255.0, 255.0}));
+		return true;
+	}
 }
 
-CimbReader::CimbReader(const cv::Mat& img, const CimbDecoder& decoder, bool needs_sharpen)
+CimbReader::CimbReader(const cv::Mat& img, CimbDecoder& decoder, bool needs_sharpen, bool color_correction)
     : _image(img)
     , _cellSize(Config::cell_size() + 2)
     , _positions(Config::cell_spacing(), Config::num_cells(), Config::cell_size(), Config::corner_padding())
@@ -53,16 +99,24 @@ CimbReader::CimbReader(const cv::Mat& img, const CimbDecoder& decoder, bool need
     , _good(_image.cols >= Config::image_size() and _image.rows >= Config::image_size())
 {
 	_grayscale = preprocessSymbolGrid(img, needs_sharpen);
+	if (_good and color_correction)
+		updateColorCorrection(_image, decoder);
 }
 
-CimbReader::CimbReader(const cv::UMat& img, const CimbDecoder& decoder, bool needs_sharpen)
-    : CimbReader(img.getMat(cv::ACCESS_READ), decoder, needs_sharpen)
+CimbReader::CimbReader(const cv::UMat& img, CimbDecoder& decoder, bool needs_sharpen, bool color_correction)
+    : CimbReader(img.getMat(cv::ACCESS_READ), decoder, needs_sharpen, color_correction)
 {
 }
 
-unsigned CimbReader::read(unsigned& bits)
+unsigned CimbReader::read_color(const PositionData& pos)
 {
-	if (_positions.done())
+	Cell color_cell(_image, pos.x, pos.y, Config::cell_size(), Config::cell_size());
+	return _decoder.decode_color(color_cell);
+}
+
+unsigned CimbReader::read(PositionData& pos)
+{
+	if (done())
 		return 0;
 
 	// need coordinate, index, and drift from next position
@@ -70,16 +124,19 @@ unsigned CimbReader::read(unsigned& bits)
 	int x = xy.first + drift.x();
 	int y = xy.second + drift.y();
 	bitmatrix cell(_grayscale, _image.cols, _image.rows, x-1, y-1);
-	Cell color_cell(_image, x-1, y-1, _cellSize, _cellSize);
 
 	unsigned drift_offset = 0;
 	unsigned error_distance;
-	bits = _decoder.decode(cell, color_cell, drift_offset, error_distance);
+	unsigned bits = _decoder.decode_symbol(cell, drift_offset, error_distance);
 
 	std::pair<int, int> best_drift = CellDrift::driftPairs[drift_offset];
 	drift.updateDrift(best_drift.first, best_drift.second);
 	_positions.update(i, drift, error_distance);
-	return i;
+
+	pos.i = i;
+	pos.x = x + best_drift.first;
+	pos.y = y + best_drift.second;
+	return bits;
 }
 
 bool CimbReader::done() const
