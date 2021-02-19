5228abfb3c824c1d870258a8ef6316c62f35ca45
sz <sz@galacticicecube.com>
2021-02-19 17:31:04 -0600

    Got a bit ambitious with reducing the thread count,
    sometimes having the extra processing capacity helps.

--- app/src/cpp/cfc-cpp/jni.cpp
+++ app/src/cpp/cfc-cpp/jni.cpp
@@ -112,7 +112,7 @@ namespace {
 	{
 		std::stringstream sstop;
 		sstop << "cfc using " << proc.num_threads() << " thread(s). " << proc.color_bits() << "..." << proc.backlog() << "? ";
-		sstop << (MultiThreadedDecoder::bytes / std::max<double>(1, MultiThreadedDecoder::decoded)) << "b v0.5.6";
+		sstop << (MultiThreadedDecoder::bytes / std::max<double>(1, MultiThreadedDecoder::decoded)) << "b v0.5.7";
 		std::stringstream ssmid;
 		ssmid << "#: " << MultiThreadedDecoder::perfect << " / " << MultiThreadedDecoder::decoded << " / " << MultiThreadedDecoder::scanned << " / " << _calls;
 		std::stringstream ssperf;
