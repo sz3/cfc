4d6ce2831bfbce0194fc847742b09e46188e6b74
sz <sz@recv.cc>
2026-02-05 17:58:53 -0600

    add save file dialog

--- app/src/main/java/org/cimbar/camerafilecopy/MainActivity.java
+++ app/src/main/java/org/cimbar/camerafilecopy/MainActivity.java
@@ -7,17 +7,25 @@ import org.opencv.core.Mat;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
 import android.view.WindowManager;
+import android.widget.CompoundButton;
 import android.widget.Toast;
+import androidx.annotation.Nullable;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends CameraActivity implements CvCameraViewListener2 {
     private static final String TAG = "cfc::MainActivity";
+    private static final int CREATE_FILE = 11;
 
     private Toast introToast;
 
@@ -60,14 +68,31 @@ public class MainActivity extends CameraActivity implements CvCameraViewListener
         setContentView(R.layout.activity_main);
 
         mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.main_surface);
-
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
-
         mOpenCvCameraView.setCvCameraViewListener(this);
 
-        // not yet for the other stuff, but toggle and whatnot goes here
+        // set up mode toggle
+        mModeSwitch = (ModeSelToggle) findViewById(R.id.mode_switch);
+        mModeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    modeVal = detectedMode;
+                } else {
+                    modeVal = 0;
+                }
+            }
+        });
         // and the hint toast
         introToast = Toast.makeText(this, "↕ Swipe to encode data! Or use cimbar.org :)",  Toast.LENGTH_LONG);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        introToast.show();
+        // reset autodetect
+        mModeSwitch.setChecked(false);
+        modeVal = 0;
     }
 
     @Override
@@ -116,8 +141,69 @@ public class MainActivity extends CameraActivity implements CvCameraViewListener
         // native call to process current camera frame
         String res = processImageJNI(mat.getNativeObjAddr(), this.dataPath, this.modeVal);
 
+        // res will contain a file path if we completed a transfer. Ask the user where to save it
+        if (res.startsWith("/")) {
+            if (res.length() == 2 && res.charAt(1) == '4') {
+                detectedMode = 4;
+            }
+            else if (res.length() == 3 && res.charAt(1) == '6' && res.charAt(2) == '6') {
+                detectedMode = 66;
+            }
+            else if (res.length() == 3 && res.charAt(1) == '6' && res.charAt(2) == '7') {
+                detectedMode = 67;
+            }
+            else {
+                detectedMode = 68;
+            }
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mModeSwitch.setChecked(true);
+                    mModeSwitch.setModeVal(detectedMode);
+                }
+            });
+
+        }
+        else if (!res.isEmpty()) {
+            Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("application/octet-stream");
+            intent.putExtra(Intent.EXTRA_TITLE, res);
+            // can't get putExtra to work for extra values, so we'll save it in the class
+            this.activePath = this.dataPath + "/" + res;
+            startActivityForResult(intent, CREATE_FILE);
+        }
+
         // return processed frame for live preview
         return mat;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (resultCode == RESULT_OK && requestCode == CREATE_FILE) {
+            if (this.activePath == null)
+                return;
+
+            // copy this.activePath (tempfile) to the user-specified location
+            try (
+                    InputStream istream = new FileInputStream(this.activePath);
+                    OutputStream ostream = getContentResolver().openOutputStream(data.getData())
+            ) {
+                byte[] buf = new byte[8192];
+                int length;
+                while ((length = istream.read(buf)) > 0) {
+                    ostream.write(buf, 0, length);
+                }
+                ostream.flush();
+            } catch (Exception e) {
+                Log.e(TAG, "failed to write file " + e.toString());
+            } finally {
+                try {
+                    new File(this.activePath).delete();
+                } catch (Exception e) {}
+                this.activePath = null;
+            }
+        }
     }
 
     private native String processImageJNI(long mat, String path, int modeInt);
